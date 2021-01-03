311b3e01ce901359de63e694acdaa039f41c6856
vudinhquang <vudinhquangk53@gmail.com>
2021-01-04 00:41:31 +0900

    Array

---
+++ typescript_training/mjs/03_variable/02-array.js
@@ -0,0 +1,6 @@
+let arrNumber = [1, 5, 8];
+let arrString = ["java", "android", "es6"];
+console.log(arrNumber);
+console.log(arrString);
+console.log(arrString.push("typescript")); // 4
+console.log(arrString); // "java", "android", "es6", "typescript"
