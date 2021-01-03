ee2baee9302ab993b1d3014ea04a654f129ab217
vudinhquang <vudinhquangk53@gmail.com>
2021-01-04 01:39:51 +0900

    Interface object

---
+++ typescript_training/mjs/03_variable/08-interface-object.js
@@ -0,0 +1,14 @@
+let objOne = {};
+console.log(objOne);
+let objTwo;
+objTwo = {
+    name: "ES6",
+    time: "5h"
+};
+let objThree = {
+    name: "Typescript",
+    time: "10h",
+    free: false
+};
+console.log(objTwo);
+console.log(objThree);
