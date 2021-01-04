6c653120f90b942057355dfd42aecc625eb33c6d
vudinhquang <vudinhquangk53@gmail.com>
2021-01-04 11:11:12 +0900

    Param 2

---
+++ typescript_training/mjs/04_function/02-param-02.js
@@ -0,0 +1,7 @@
+function totalLength(x, y) {
+    return x.length + y.length;
+}
+// totalLength(['abc', 'def'], ['123']);
+console.log(totalLength('abc', ['123'])); // 4
+console.log(totalLength([1, "abc", "def"], ['123', "def"])); // 5
+console.log(totalLength([1, "abc", "def"], "123")); // 3 + 3
