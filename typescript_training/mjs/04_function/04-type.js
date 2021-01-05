5df5178f9b8d0e9c60e6b409bc41d8b349854fb9
vudinhquang <vudinhquangk53@gmail.com>
2021-01-06 00:10:11 +0900

    Function types

---
+++ typescript_training/mjs/04_function/04-type.js
@@ -0,0 +1,9 @@
+function userInfo01(name, age) {
+    return `My name is ${name}, ${age} years old`;
+}
+let userInfo02 = function (name, age) {
+    return `My name is ${name}, ${age} years old`;
+};
+
+console.log(userInfo01("abc", 23));
+console.log(userInfo02("abc", 23));
