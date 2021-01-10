29ad0b30b324df5fdb2e400570b06b9f19a72a34
vudinhquang <vudinhquangk53@gmail.com>
2021-01-10 16:57:54 +0900

    Method

---
+++ typescript_training/mjs/06_generic/02-method.js
@@ -0,0 +1,8 @@
+class StudyGeneric {
+    static printArray(params) {
+        console.log(params);
+    }
+}
+StudyGeneric.printArray([1, 2, 3, 5]);
+StudyGeneric.printArray(["a", "bc", "def"]);
+StudyGeneric.printArray([1, 2, 3, 5, "a", false, "bc", "def", true]);
