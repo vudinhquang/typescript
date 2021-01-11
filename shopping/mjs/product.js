8d6a21b700f1336b234e2f152adafd6e92baaa9d
vudinhquang <vudinhquangk53@gmail.com>
2021-01-12 02:12:03 +0900

    Product

---
+++ shopping/mjs/product.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    get id() {
+        return this._id;
+    }
+    set id(v) {
+        this._id = v;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(v) {
+        this._name = v;
+    }
+    get image() {
+        return this._image;
+    }
+    set image(v) {
+        this._image = v;
+    }
+    get summary() {
+        return this._summary;
+    }
+    set summary(v) {
+        this._summary = v;
+    }
+    get price() {
+        return this._price;
+    }
+    set price(v) {
+        this._price = v;
+    }
+    get canBuy() {
+        return this._canBuy;
+    }
+    set canBuy(v) {
+        this._canBuy = v;
+    }
+}
+exports.Product = Product;
