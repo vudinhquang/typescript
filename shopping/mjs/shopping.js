17fc638578df4b48279386516ed8e77e303d5f05
vudinhquang <vudinhquangk53@gmail.com>
2021-01-14 03:22:29 +0900

    Empty Cart

--- shopping/mjs/shopping.js
+++ shopping/mjs/shopping.js
@@ -6,11 +6,29 @@ let products = productRepository.getItems();
 var MElement;
 (function (MElement) {
     MElement.ELM_LIST_PRODUCT = "#list-product";
+    MElement.ELM_NOTIFICATION = "#mnotification";
+    MElement.ELM_CART_BODY = "#my-cart-body";
+    MElement.ELM_CART_FOOTER = "#my-cart-footer";
 })(MElement || (MElement = {}));
+var MNotification;
+(function (MNotification) {
+    MNotification.NOTI_READY_TO_BUY = "Ready to buy product";
+})(MNotification || (MNotification = {}));
 // Hiển thị danh sách sản phẩm
 function showListProduct() {
     $(MElement.ELM_LIST_PRODUCT).html(productRepository.showItemsInHTML());
 }
+// Hiển thị thông báo
+function showNotification(str) {
+    $(MElement.ELM_NOTIFICATION).html(str);
+}
+// Hiển thị giỏ hàng:
+function showCart() {
+    $(MElement.ELM_CART_BODY).html("");
+    $(MElement.ELM_CART_FOOTER).html("");
+}
 jQuery(function () {
     showListProduct();
+    showCart();
+    showNotification(MNotification.NOTI_READY_TO_BUY);
 });
--- shopping/ts/shopping.ts
+++ shopping/ts/shopping.ts
@@ -5,7 +5,14 @@ let productRepository = new ProductRepository();
 let products : Product[] =  productRepository.getItems();
 
 namespace MElement {
-	export const ELM_LIST_PRODUCT : string = "#list-product";
+    export const ELM_LIST_PRODUCT : string = "#list-product";
+    export const ELM_NOTIFICATION : string = "#mnotification";
+	export const ELM_CART_BODY: string = "#my-cart-body";
+	export const ELM_CART_FOOTER: string = "#my-cart-footer";
+}
+
+namespace MNotification {
+	export const NOTI_READY_TO_BUY : string = "Ready to buy product";
 }
 
 // Hiển thị danh sách sản phẩm
@@ -13,6 +20,19 @@ function showListProduct() : void{
 	$(MElement.ELM_LIST_PRODUCT).html(productRepository.showItemsInHTML());
 }
 
+// Hiển thị thông báo
+function showNotification(str: string) : void{
+	$(MElement.ELM_NOTIFICATION).html(str);
+}
+
+// Hiển thị giỏ hàng:
+function showCart() : void{
+	$(MElement.ELM_CART_BODY).html("");
+	$(MElement.ELM_CART_FOOTER).html("");
+}
+
 jQuery(function() { 
     showListProduct();
+    showCart();
+    showNotification(MNotification.NOTI_READY_TO_BUY);
 })
